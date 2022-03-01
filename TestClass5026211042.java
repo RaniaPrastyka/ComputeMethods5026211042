8f8b3f2e2edd9dd61aaf0ff4f56e558c8234d960
RaniaPrastyka <100117627+RaniaPrastyka@users.noreply.github.com>
2022-03-01 08:59:18 +0700

    Add files via upload

---
+++ TestClass5026211042.java
@@ -0,0 +1,13 @@
+public class TestClass5026211042{
+
+	public static void main(String[] args){
+		ComputeMethods5026211042 rp = new ComputeMethods5026211042();
+
+		System.out.println("Temp in Celcius is "+rp.fToC(78));
+
+		System.out.println("Hypotenuse is " +rp.hypotenuse(7,8));
+
+		System.out.println("The sum of the dice values is "+rp.roll());
+	
+	}
+}
